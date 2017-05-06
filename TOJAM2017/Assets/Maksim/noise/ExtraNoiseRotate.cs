c2b1493c41dd8153d8e38d59871ffd20fe968390
maksch <maksim.sen@hotmail.co.il>
2017-05-06 17:27:09 -0400

    noise shaders

---
+++ TOJAM2017/Assets/Maksim/noise/ExtraNoiseRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraNoiseRotate : MonoBehaviour {
+    public float intensity;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, intensity, 0);
+	}
+}
